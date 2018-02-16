1f9d1f3cc59f7e44c713437db7ee2d94f5c31075
Jonathan Vargas <jonathanv3232@gmail.com>
2018-02-15 23:56:49 -0500

    fixes the page navigation; the app is now full functional at a basic level

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
 const readline = require('readline');
-
 const newCarResults = [];
 
 const rl = readline.createInterface({
@@ -23,18 +22,18 @@ async function getCars(make, model) {
   console.log(message);
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080 });
   const url = `https://www.copart.com/lotSearchResults/?free=true&query=${make}%20${model}`;
   console.log(url);
   await page.goto(url);
-  await page.setViewport({ width: 1920, height: 1080 });
   await page.waitFor(2000);
-  page.click('#lot_year');
-  await page.waitFor(3500);
-  await page.screenshot({ path: 'screenshots/shot.png' });
+  await page.click('#lot_year');
+  await page.waitFor(3000);
+  // await page.screenshot({ path: 'screenshots/shot.png' });
 
   // gets information about the results that we got back from our search
   const resultDetails = await page.evaluate(function() {
-    // takes a part of the string containing the number of results and then parses it
+    //takes a part of the string containing the number of results and then parses it
     const dataTableInfo = '#serverSideDataTable_info';
     const numberOfResultsString = document.querySelector(dataTableInfo).innerText.split(' ')[5];
     const numberOfResults = parseInt(numberOfResultsString.replace(',', ''));
@@ -42,50 +41,50 @@ async function getCars(make, model) {
     const currentlyDisplaying = document.querySelector('#serverSideDataTable > tbody').childElementCount;
 
     const pages = Math.ceil(numberOfResults / currentlyDisplaying);
+
     return {
-      numberOfResults: numberOfResults,
       currentlyDisplaying: currentlyDisplaying,
+      numberOfResults: numberOfResults,
       pages: pages,
     };
   });
-
+  console.log(resultDetails);
   // if it is a really commmon car then we will need to continue on to the next page.
-  for (let i = 0; i < resultDetails.pages; i++) {
-    for (let j = 0; j < resultDetails.currentlyDisplaying; j++) {
-      carDetails = await page.evaluate(
-        function(index, page) {
-          const carYearSelector = `#serverSideDataTable > tbody > tr:nth-child(${index}) > td:nth-child(4) > span:nth-child(1)`;
-          const carMakeSelector = `#serverSideDataTable > tbody > tr:nth-child(${index}) > td:nth-child(5) > span`;
-          const carModelSelector = `#serverSideDataTable > tbody > tr:nth-child(${index}) > td:nth-child(6) > span`;
-          const damageTypeSelector = `#serverSideDataTable > tbody > tr:nth-child(${index}) > td:nth-child(12) > span`;
-          const carYear = document.querySelector(carYearSelector).innerText;
-          const carMake = document.querySelector(carMakeSelector).innerText;
-          const carModel = document.querySelector(carModelSelector).innerText;
-          const damageType = document.querySelector(damageTypeSelector).innerText;
-          const info = {
-            carMake: carMake,
-            carModel: carModel,
-            carYear: carYear,
-            damageType: damageType,
-          };
-          return info;
-        },
-        j,
-        page
-      );
+  outerloop: for (let i = 0; i < resultDetails.pages; i++) {
+    console.log(`On page ${i + 1}`);
+    for (let j = 1; j <= resultDetails.currentlyDisplaying; j++) {
+      let carDetails = await page.evaluate(function(index) {
+        const carSelector = `#serverSideDataTable > tbody > tr:nth-child(${index})`;
+        const carYearSelector = `${carSelector} > td:nth-child(4) > span:nth-child(1)`;
+        const carMakeSelector = `${carSelector} > td:nth-child(5) > span`;
+        const carModelSelector = `${carSelector} > td:nth-child(6) > span`;
+        const damageTypeSelector = `${carSelector} > td:nth-child(12) > span`;
+
+        const carYear = document.querySelector(carYearSelector).innerText;
+        const carMake = document.querySelector(carMakeSelector).innerText;
+        const carModel = document.querySelector(carModelSelector).innerText;
+        const damageType = document.querySelector(damageTypeSelector).innerText;
+        const info = {
+          carMake: carMake,
+          carModel: carModel,
+          carYear: carYear,
+          damageType: damageType,
+        };
+        console.log(info);
+        return info;
+      }, j);
 
       if (carDetails.carYear >= 2014) {
         newCarResults.push(carDetails);
       } else {
-        break;
+        break outerloop;
       }
+      console.log(carDetails);
     }
-
     await page.click('#serverSideDataTable_next > a');
     console.log('Going to the next page, give me a sec.');
-    await page.waitFor(4000);
+    await page.waitFor(2500);
   }
-
-  console.log(newCarResults.length);
+  console.log(`We have ${newCarResults.length} results in total`);
   await browser.close();
 }
